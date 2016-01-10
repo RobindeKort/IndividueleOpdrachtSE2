5098ca1d5b8ec57429abebf09d7fbaf714bb00e5
Kort,Robin R.A.L.F. de <Robin.dekort@student.fontys.nl>
2016-01-10 07:07:24 +0100

    Display Loggedin

--- Pages/Index.aspx
+++ Pages/Index.aspx
@@ -1,8 +1,10 @@
-﻿<%@ Page Title="Home" Language="C#" MasterPageFile="~/Pages/LeagueOfLegends.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="IndividueleOpdrachtSE2.Index1" %>
+﻿<%@ Page Title="Home" Language="C#" MasterPageFile="~/Pages/LeagueOfLegends.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="IndividueleOpdrachtSE2.Pages.Index" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="css" runat="server">
     <link rel="stylesheet" href="/css/Index.css" />
 </asp:Content>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="CPHBody" runat="server">
-
+    <div class="container jumbotron">
+        <asp:PlaceHolder ID="name" runat="server"/>
+    </div>
 </asp:Content>
--- Pages/Index.aspx.cs
+++ Pages/Index.aspx.cs
@@ -4,16 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
-namespace IndividueleOpdrachtSE2
+namespace IndividueleOpdrachtSE2.Pages
 {
-    public partial class Index1 : System.Web.UI.Page
+    public partial class Index : System.Web.UI.Page
     {
-        Administratie admin;
+        private Administratie admin;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             admin = new Administratie();
+
+            ShowName();
+        }
+
+        private void ShowName()
+        {
+            string loginName = (string)Session["loginName"];
+            if (loginName != null)
+            {
+                foreach (User u in admin.Users)
+                {
+                    if (u.LoginName == loginName)
+                    {
+                        HtmlGenericControl p = new HtmlGenericControl("p");
+                        p.InnerText = u.ToString();
+                        name.Controls.Add(p);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
